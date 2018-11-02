a17ea2b78b6c09b5cc419ab0743772c79ba168db
Fabian Gutierrez <fega.hg@gmail.com>
2018-11-01 20:07:11 -0500

    Added a better email link template

--- routes/util.js
+++ routes/util.js
@@ -4,20 +4,3 @@
  */
 exports.asyncController = asyncFn => (req, res, next) => Promise
   .resolve(asyncFn(req, res, next)).catch(next);
-
-exports.htmlEmailTemplate = (path, body) => `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-  <title>Document</title>
-</head>
-<body>
-  <h1>Somebody send a POST ${path}</h1>
-  <h2>Body</h2>
-  <pre style="background-color:#eee;padding:10px">${JSON.stringify(body, null, 2)}</pre>
-</body>
-</html>
-`;
